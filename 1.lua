41a1d79cdb1e9c5578d1a23e570281aa1c1bf955
AdamFatherOfCreator <140255333+AdamFatherOfCreator@users.noreply.github.com>
2024-07-05 14:52:57 +0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,40 +1,26 @@
-       function UpdatePlayerChams()
-            for i,v in pairs(game:GetService'Players':GetChildren()) do
-                pcall(function()
-                    if not isnil(v.Character) then
-                        if ESPPlayer then
-                            if not isnil(v.Character.Head) and not v.Character.Head:FindFirstChild('NameEsp'..Number) then
-                                local bill = Instance.new('BillboardGui',v.Character.Head)
-                                bill.Name = 'NameEsp'..Number
-                                bill.ExtentsOffset = Vector3.new(0, 1, 0)
-                                bill.Size = UDim2.new(1,200,1,30)
-                                bill.Adornee = v.Character.Head
-                                bill.AlwaysOnTop = true
-                                local name = Instance.new('TextLabel',bill)
-                                name.Font = "SourceSansBold"
-                                name.FontSize = "Size14"
-                                name.TextWrapped = true
-                                name.Text = (v.Name ..' \n'.. round((game:GetService('Players').LocalPlayer.Character.Head.Position - v.Character.Head.Position).Magnitude/3) ..' M')
-                                name.Size = UDim2.new(1,0,1,0)
-                                name.TextYAlignment = 'Top'
-                                name.BackgroundTransparency = 1
-                                name.TextStrokeTransparency = 0.5
-                                if v.Team == game.Players.LocalPlayer.Team then
-                                    name.TextColor3 = Color3.new(0, 254, 252)
-                                else
-                                    name.TextColor3 = Color3.new(0, 254, 252)
-                                end
-                            else
-                                v.Character.Head['NameEsp'..Number].TextLabel.Text = (v.Name ..'   \n'.. round((game:GetService('Players').LocalPlayer.Character.Head.Position - v.Character.Head.Position).Magnitude/3) ..' M')
-                            end
-                        else
-                            if v.Character.Head:FindFirstChild('NameEsp'..Number) then
-                                v.Character.Head:FindFirstChild('NameEsp'..Number):Destroy()
-                            end
-                        end
-                    end
-                end)
-            end
-        end
-ESPPlayer = true
-UpdatePlayerChams()
+spawn(function()
+       while wait() do
+              pcall(function()
+if _G.Hitboxplr then
+                                          
+for i,v in pairs(game.Players:GetChildren()) do
+if v.Name ~= game.Players.LocalPlayer.Name then
+v.Character.HumanoidRootPart.Size = Vector3.new(fathitbox, fathitbox, fathitbox)
+v.Character.HumanoidRootPart.Transparency = 0.7
+v.Character.HumanoidRootPart.Color = Color3.fromRGB(0, 255, 127)
+end
+end
+                                          
+else
+                                          
+for i,v in pairs(game.Players:GetChildren()) do
+if v.Name ~= game.Players.LocalPlayer.Name then
+v.Character.HumanoidRootPart.Transparency = 1
+v.Character.HumanoidRootPart.Size = Vector3.new(2, 2, 1)
+end
+end
+                                          
+end
+              end)
+       end
+end)

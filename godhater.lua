755382db9a79f3fc7d9621228b3b23da77fd794a
PotatoNotTomato <140255333+PotatoEiii@users.noreply.github.com>
2024-08-18 20:47:05 +0700

    Update godhater.lua

--- godhater.lua
+++ godhater.lua
@@ -2855,3 +2855,38 @@ for theme, color in pairs(colors) do
     end)
 end
 end
+
+ UIS = game:GetService('UserInputService')
+ frame = game.CoreGui.ScreenGui.SalunaDevoz.Main
+ dragToggle = nil
+ dragSpeed = 0.05
+ dragStart = nil
+ startPos = nil
+
+ function updateInput(input)
+  delta = input.Position - dragStart
+  position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+  startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+ game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+end
+
+frame.InputBegan:Connect(function(input)
+ if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+  dragToggle = true
+  dragStart = input.Position
+  startPos = frame.Position
+  input.Changed:Connect(function()
+   if input.UserInputState == Enum.UserInputState.End then
+    dragToggle = false
+   end
+  end)
+ end
+end)
+
+UIS.InputChanged:Connect(function(input)
+ if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+  if dragToggle then
+   updateInput(input)
+  end
+ end
+end)
